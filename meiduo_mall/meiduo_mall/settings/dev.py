0122835f8239d7c73c229f65cdfd7d50e3ebc23f
fgsama <cyber@fgsama.vir>
2021-06-09 13:18:51 +0800

    configure mysql

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -91,9 +91,17 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'cyber',
+        'PASSWORD': '12345678',
+        'NAME': 'meiduo',
     }
 }
 
