5ebd1146bcbdb316c4db075332bfd4f52cedce02
fgsama <cyber@fgsama.vir>
2021-06-13 14:26:13 +0800

    init registration page

---
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -0,0 +1,99 @@
+ let vm = new Vue({
+    el: '#app', // id selector
+    delimiters: ['[[',']]'],
+    data: {
+        // v-model
+        username: '',
+        password: '',
+        password2: '',
+        mobile: '',
+        allow: '',
+
+        // v-show
+        error_name: false,
+        error_password: false,
+        error_password2: false,
+        error_mobile: false,
+        error_allow: false,
+
+        // error_message
+        error_name_message: '',
+        error_mobile_message: '',
+
+    },
+    methods: {
+        check_username() {
+//                console.log(222)
+//                this.error_name_message = 'nice'
+//                this.error_name = true
+//               if (this.username != '530'){
+//                this.error_name_message = '530';
+//                this.error_name = true;
+//               }
+//            let re = /^[a-zA-Z0-9_-]{5,20}$/;
+//            if (re.test(this.username)){
+//                this.error_name = false;
+//            }else{
+//                this.error_name_message = '530';
+//                this.error_name = true;
+//            }
+        let re = /^[a-zA-Z0-9_-]{5,20}$/;
+        if (re.test(this.username)) {
+            this.error_name = false;
+        } else {
+            this.error_name_message = '请输入5-20个字符的用户名';
+            this.error_name = true;
+        }
+    },
+
+//
+        check_password(){
+            let re = /^[0-9A-Za-z]{8,20}$/;
+                if (re.test(this.password)) {
+                    this.error_password = false;
+                } else {
+                    this.error_password = true;
+                }
+        },
+
+        check_password2(){
+            if(this.password != this.password2) {
+                this.error_password2 = true;
+            } else {
+                this.error_password2 = false;
+            }
+        },
+
+        check_mobile(){
+            let re = /^1[3-9]\d{9}$/;
+            if(re.test(this.mobile)) {
+                this.error_mobile = false;
+            } else {
+                this.error_mobile_message = '您输入的手机号格式不正确';
+                this.error_mobile = true;
+            }
+        },
+
+        check_allow(){
+            if(!this.allow) {
+                this.error_allow = true;
+            } else {
+                this.error_allow = false;
+            }
+        },
+
+        on_submit(){
+            this.check_username();
+            this.check_password();
+            this.check_password2();
+            this.check_mobile();
+            this.check_allow();
+
+            if(this.error_name == true || this.error_password == true || this.error_password2 == true
+                || this.error_mobile == true || this.error_allow == true) {
+                // 禁用表单的提交
+                window.event.returnValue = false;
+        }
+        }
+        }
+ });
