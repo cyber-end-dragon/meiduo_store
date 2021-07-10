8fa3695f1aded5ba42d212912442513f921d3bd6
fgsama <cyber@fgsama.vir>
2021-07-10 11:21:00 +0800

    init user login

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -9,17 +9,62 @@ from django_redis import get_redis_connection
 from users.models import User
 from meiduo_mall.utils.response_code import RETCODE
 from django.db import DatabaseError
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 
-class UsernameCountView(View):
+class LoginView(View):
 
+    # 提供用户登陆页面
+    def get(self, request):
+
+        return render(request, 'login.html')
+
+    # 实现用户登陆逻辑
+    def post(self, request):
+        # 接收参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        remembered = request.POST.get('remembered')
+
+        # 校验参数
+        if not all([username, password]):
+            return http.HttpResponseForbidden('缺少参数')
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 认证用户
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return render(request, 'login.html', {'account_errmsg': '账号或密码错误'})
+        # 状态保持
+        login(request, user)
+        if remembered != 'on':
+            # 没有记住登陆： 状态保持在浏览器会话结束后销毁
+            request.session.set_expiry(0)
+        else:
+            # 记住登陆： 状态默认保持周期为两周
+            request.session.set_expiry(None)
+        # 响应结果
+        return redirect(reverse('contents:index'))
+        pass
+
+
+class MobileCountView(View):
+    # 手机号重复注册
+    def get(self, request, mobile):
+
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
+
+class UsernameCountView(View):
+    # 用户名重复注册
     def get(self, request, username):
 
         count = User.objects.filter(username=username).count()
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
-
 
 
 class RegisterView(View):
@@ -28,7 +73,7 @@ class RegisterView(View):
 
         return render(request, 'register.html')
 
-    def post(self,request):
+    def post(self, request):
 
         # 接收参数
         username = request.POST.get('username')
@@ -39,7 +84,7 @@ class RegisterView(View):
         allow = request.POST.get('allow')
 
         # 校验参数
-        if not all([username,password,password2,mobile,allow]):
+        if not all([username, password, password2, mobile, allow]):
             return http.HttpResponseForbidden('缺少参数 Missing Parameter')
         if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
             return http.HttpResponseForbidden('请输入5-20个字符的用户名')
@@ -47,7 +92,7 @@ class RegisterView(View):
             return http.HttpResponseForbidden('请输入8-20位的密码')
         if password != password2:
             return http.HttpResponseForbidden('两次输入的密码不一致')
-        if not re.match(r'^1[3-9]\d{9}$',mobile):
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
             return http.HttpResponseForbidden('您输入的手机号格式不正确')
         if allow != 'on':
             return http.HttpResponseForbidden('请勾选用户协议')
@@ -65,9 +110,8 @@ class RegisterView(View):
         try:
             user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except DatabaseError:
-            return render(request, 'register.html', {'register_errmsg':'注册失败'})
-        login(request,user)
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+        login(request, user)
 
         # return http.HttpResponse('注册成功')
         return redirect(reverse('contents:index'))
-
--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -159,6 +159,24 @@
                 this.error_mobile_message = '您输入的手机号格式不正确';
                 this.error_mobile = true;
             }
+            // 判断手机号是否重复注册
+            if (this.error_mobile == false) {
+                let url = '/mobiles/' + this.mobile + '/count/';
+                axios.get(url, {
+                    responseType: 'json'
+                })
+                    .then(response => {
+                        if (response.data.count == 1) {
+                            this.error_mobile_message = '手机号已存在';
+                            this.error_mobile = true;
+                        } else {
+                            this.error_mobile = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response);
+                    })
+            }
         },
 
         check_image_code(){
